fd19023a2fdd2d4bc530e057f4ff0549f6bbabbe
Pierrick Varin <pierrick.varin@glitchstudio.com>
2017-01-19 01:41:05 +0100

    Added cache busting parameters to css links in index html

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 	<title>Hezad's website</title>
 
 	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
-	<link href='style.css' rel='stylesheet' type='text/css'>
-	<link href='mobile.css' rel='stylesheet' type='text/css'>
+	<link href='style.css?20160714' rel='stylesheet' type='text/css'>
+	<link href='mobile.css?20160714' rel='stylesheet' type='text/css'>
 
 	<script src="jquery.js"></script>
 
